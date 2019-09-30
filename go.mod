039e50a91c8474ad1e4269131b61c6fc18e024c5
Jeff Lee <jeffomatic@gmail.com>
2019-09-29 17:20:21 -0700

    implement Slack Events API URL verification

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jeffomatic/reacjirouter
 
 go 1.13
+
+require (
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.3
+)
--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
+type slackEvent struct {
+	T string `json:"type"`
+
+	// for URL verification
+	Token     string
+	Challenge string
+}
+
+func handleSlackEvent(w http.ResponseWriter, r *http.Request) {
+	v, ok := r.Header["Content-Type"]
+	if !ok || v[0] != "application/json" {
+		fmt.Println(r.Header)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var e slackEvent
+	err := decoder.Decode(&e)
+	if err != nil {
+		log.Println("error decoding JSON:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	switch e.T {
+	case "url_verification":
+		fmt.Fprintf(w, e.Challenge)
+
+	default:
+		// do nothing
+	}
+}
+
 func main() {
-	http.ListenAndServe(":1234", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	}))
+	router := mux.NewRouter()
+	router.Path("/slack/event").Methods("POST").HandlerFunc(handleSlackEvent)
+
+	handler := handlers.LoggingHandler(os.Stdout, router)
+
+	port := 1234
+	fmt.Println("Starting server on port", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
