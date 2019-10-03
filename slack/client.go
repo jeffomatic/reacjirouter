15b5a485a8870f624dc693adbb1f39f12b69ddeb
Jeff Lee <jeffomatic@gmail.com>
2019-10-03 00:10:59 -0700

    slack: factor out client response handling

--- slack/client.go
+++ slack/client.go
@@ -21,6 +21,39 @@ func NewClient(token string) *Client {
 	return &Client{URLPrefix: defaultURLPrefix, AccessToken: token}
 }
 
+func handleJSONResponse(resp *http.Response, respBody interface{}) error {
+	if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return errors.Wrap(err, "reading HTTP response body")
+	}
+
+	errCheck := struct {
+		Ok    bool
+		Error string
+	}{}
+	err = json.Unmarshal(respBytes, &errCheck)
+	if err != nil {
+		return errors.Wrap(err, "response body error-check decode")
+	}
+	if !errCheck.Ok {
+		return fmt.Errorf("Slack API error: %s", errCheck.Error)
+	}
+
+	if respBody != nil {
+		err = json.Unmarshal(respBytes, respBody)
+		if err != nil {
+			return errors.Wrap(err, "response body unmarshal")
+		}
+	}
+
+	return nil
+}
+
 func (c *Client) Call(method string, body interface{}, respBody interface{}) error {
 	reqBody := []byte(`{}`)
 	var err error
@@ -48,33 +81,8 @@ func (c *Client) Call(method string, body interface{}, respBody interface{}) err
 	if err != nil {
 		return errors.Wrap(err, "executing HTTP request")
 	}
-	if resp.StatusCode/100 != 2 {
-		return fmt.Errorf("bad status: %s", resp.Status)
-	}
 
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return errors.Wrap(err, "reading HTTP response body")
-	}
+	return handleJSONResponse(resp, respBody)
+}
 
-	errCheck := struct {
-		Ok    bool
-		Error string
-	}{}
-	err = json.Unmarshal(respBytes, &errCheck)
-	if err != nil {
-		return errors.Wrap(err, "response body error-check decode")
-	}
-	if !errCheck.Ok {
-		return fmt.Errorf("Slack API error: %s", errCheck.Error)
-	}
-
-	if respBody != nil {
-		err = json.Unmarshal(respBytes, respBody)
-		if err != nil {
-			return errors.Wrap(err, "response body unmarshal")
-		}
-	}
-
-	return nil
 }
