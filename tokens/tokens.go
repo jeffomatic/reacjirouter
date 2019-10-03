e7ea6ec7b61938ee20bdd3b78e89ebf6b9a03a31
Jeff Lee <jeffomatic@gmail.com>
2019-10-03 00:09:24 -0700

    add token store

---
+++ tokens/tokens.go
@@ -0,0 +1,26 @@
+package tokens
+
+import "sync"
+
+var (
+	teamCredsMutex sync.Mutex
+	teamCreds      map[string]string
+)
+
+func init() {
+	teamCreds = make(map[string]string)
+}
+
+func Add(teamID string, accessToken string) {
+	teamCredsMutex.Lock()
+	defer teamCredsMutex.Unlock()
+
+	teamCreds[teamID] = accessToken
+}
+
+func Get(teamID string) (string, bool) {
+	teamCredsMutex.Lock()
+	defer teamCredsMutex.Unlock()
+	accessToken, ok := teamCreds[teamID]
+	return accessToken, ok
+}
